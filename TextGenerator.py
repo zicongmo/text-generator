695ee362dfe3b9279407d4cef8919a26cf34bac0
Joseph Mo <josephmo@Josephs-MacBook-Pro.local>
2017-10-10 20:05:33 -0700

    Added ability to generate text

--- TextGenerator.py
+++ TextGenerator.py
@@ -1,19 +1,20 @@
-from Node import Node
+from NodeList import NodeList
 from random import sample
 
 
-# Generates up to len words of text using the given node list
+# Generates up to len words of text using the given node_list
 # Stops if there are no more connections that can be followed
-def generate_text(node_list, length=100):
+def generate_text(text, length=100, node_length=2):
+    node_list = NodeList(text, node_length)
     current_node = sample(node_list)
     generated_text = []
-    for i in range(Node.length):
+    for i in range(node_length):
         if i == length:
             break
         generated_text.append(current_node.words[i])
-    for i in range(length - Node.length):
+    for i in range(length - node_length):
         current_node = current_node.next_node()
-        generated_text.append(current_node.words[Node.length-1])
+        generated_text.append(current_node.words[node_length-1])
     return generated_text
 
 try:
@@ -22,3 +23,5 @@ try:
     print(data)
 except FileNotFoundError:
     print("Sorry! File was not found")
+
+
